8aa344f81aca6b06c2fe75b85171c6a150fd22f1
3x!1 <rabyejlassi@gmail.com>
2023-04-04 16:13:19 +0000

    feat: Update source code repository URL

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,7 @@ fn about_me() -> Html {
                     style="text-transform: uppercase; color: #07d404; text-decoration: none;"
                     target="_blank"
                     title="The source code of this page"
-                    href="https://github.com/exilvm/exilvm/tree/site"
+                    href="https://github.com/exilvm/exilvmlinuz"
                     rel="noreferrer"
                 >
                     { "3x!1" }
